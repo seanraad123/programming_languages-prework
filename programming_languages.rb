36f401313a9b368df3519986faf6f8c74e2b7f05
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 01:06:10 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -59,12 +59,12 @@ def reformat_languages(languages)
   end
 
   #Add ruby to the new hash
-  #new_hash[programming_languages[0]] = {type: type[0]}
-  #new_hash.merge!(style: style[0])
+  new_hash[programming_languages[0]] = {type: type[0]}
+  new_hash.merge!(style: style[0])
 
-  #Add ruby to the new hash
-  new_hash[programming_languages[1]] = {type: type[0]}
-  new_hash.merge!(style: style)
+  #Add javascript to the new hash
+  #new_hash[programming_languages[1]] = {type: type[0]}
+  #new_hash.merge!(style: style)
 
   print new_hash
   #print programming_languages
