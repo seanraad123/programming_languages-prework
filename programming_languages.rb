1ee0ca222efef41345cd6f87c862d7e78160aaef
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 00:44:35 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -30,23 +30,23 @@ languages = {
   }
 }
 
+
+hash = {}
+hash[:my_key] = "my value"
+hash = {}
+hash[:my_key] = {second_level_key: "second level value"}
+
 def reformat_languages(languages)
   style = []
   programming_languages = []
   new_hash = {}
   type = ["interpreted", "compiled"]
 
-  hash = {}
-  hash[:my_key] = "my value"
-
-
-  hash = {}
-  hash[:my_key] = {second_level_key: "second level value"}
 
   languages[:oo].each do |language, atrributes|
     programming_languages.push(language)
   end
-  print language
+  print programming_languages
 
 
 end
