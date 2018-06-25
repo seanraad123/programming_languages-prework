40fb4f40040cf79a0b9f0b0d0a404dcf7eb3865d
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 01:03:17 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -65,7 +65,7 @@ def reformat_languages(languages)
 
   #Add ruby to the new hash
   new_hash[programming_languages[1]] = {type: type[0]}
-  new_hash.merge!(style: style[0])
+  new_hash.merge!(style: style)
 
   print new_hash
   #print programming_languages
