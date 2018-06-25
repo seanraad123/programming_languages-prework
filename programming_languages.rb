8979c4d782a4b54bf16324e816e55c6334060669
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 00:35:24 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -26,7 +26,7 @@ languages = {
     :javascript => {
       :type => "interpreted"
     }
- 
+
   }
 }
 
@@ -37,8 +37,8 @@ def reformat_languages(languages)
 
   #hash = {}
   #hash[:my_key] = {second_level_key: "second level value"}
-  
-  languages.each[:oo] do |x|
+
+  languages[:oo].each do |x|
     print x
   end
 
