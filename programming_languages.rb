56a636933c5d14486d38d125679851c62da02417
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 00:53:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -52,7 +52,7 @@ def reformat_languages(languages)
 
   #print programming_languages
 
-  new_hash[programming_languages[0]] = {type: type[0]}
+  new_hash[programming_languages[0]] = {type: type[0]}{style: style[0]}
   print new_hash
 
 end
