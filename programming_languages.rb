467bedb1bb92969e952ea4ff7405296efba1cbb5
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 00:24:44 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,12 @@
 def reformat_languages(languages)
   #hash = {}
   #hash[:my_key] = "my value"
-  
+
+
+  #hash = {}
+  #hash[:my_key] = {second_level_key: "second level value"}
+
+
   new_hash = {}
 
 end
